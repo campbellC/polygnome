b5250cd8b755d0bea398e87f309b10c3c4694e1c
Chris Campbell <chriscampbell19@users.noreply.github.com>
2015-03-03 10:27:11 +0000

    playing around with qDefs to find linear dependences

--- qDefs.py
+++ qDefs.py
@@ -28,7 +28,7 @@ x4 = mono(["x4"])
 zero = mono([],coefficient({'':0}))
 one = mono()
 
-variables = [x1,x2,x3,x4]
+variables = [x2,x1,x3,x4]
 letterCoeffs = []
 for i in 'abcdefghijklmnop':
     letterCoeffs.append(coefficient({i:1}))
@@ -42,10 +42,10 @@ for inum,i in enumerate(letterCoeffs):  #this for loop builds a generic function
 # Now to write the map from such a function to K2. That is, it takes a vector of size four and returns a vector
 # of size six, each one being what it does to that relation
 def vectToLatex(vect):
-    print '\\( \\begin{array}{c}'
+    print '\\left( \\begin{array}{c}'
     for i in vect:
-        print i, '//'
-    print '\\end{array} \\)'
+        print i, '\\\\'
+    print '\\end{array} \\right)'
 
 def k1(vect):
     assert len(vect) == 4
@@ -57,13 +57,39 @@ def k1(vect):
     answer[4] = answer[4] + x3*vect[1] - x1*vect[3]*q+ vect[2] * x2 - vect[0] * x4*q
     answer[5] = answer[5] + x4*vect[2] - x1*vect[3] +  vect[3] * x3 - vect[0] * x4
     return answer
+def k2(vect):
+    answer = [zero,zero,zero,zero]
+    answer[0] = x3 * vect[3] - x1 * vect[4] + x1 * q * vect[5]
+    answer[1] = x4 * vect[0] - x1 * vect[2]
+    answer[2] = x4 * vect[4] - x1 * vect[1]
+    answer[3] = x4 * vect[3] - x2 * vect[4] + x2 * q * vect[5]
+    return answer
+
+
+print 'From here are the images of k1\n \('
 j = -1
+n = 0
 for i in range(16):
     genericVectFirstPosition = [zero,zero,zero,zero]
     if i % 4 == 0:
         j = j + 1
     genericVectFirstPosition[j] = genericVectFirstPosition[j] + variables[i % 4]
-    vectToLatex(k1(genericVectFirstPosition))
+#   vectToLatex(k1(genericVectFirstPosition))
+    for i in k1(genericVectFirstPosition)[1].monos:
+        print i,i.coeff.coeffs
+    n = n+ 1 
+#   if n % 4 == 0:
+#        print '\\) \n\n \\('
 
-
-
+#print '\)'
+print 'From here are the images of k2\n \('
+n = 0
+for i in range(6):
+    for j in range(4):
+        for k in range(j,4):
+            genericVectFirstPosition = [zero,zero,zero,zero,zero,zero]
+            genericVectFirstPosition[j] = genericVectFirstPosition[j] + variables[j] * variables[k]
+#           vectToLatex(k2(genericVectFirstPosition))
+        
+#        print '\\)\n \n \\('
+#print '\)'
