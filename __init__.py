cb0abd60ac54550862d5cf1142d8b572698a2d51
Chris Campbell <chriscampbell19@users.noreply.github.com>
2016-01-15 14:28:19 +0000

    changed init file to better suit how i work with polygnome'

--- __init__.py
+++ __init__.py
@@ -7,4 +7,5 @@ from relation import relation
 from algebra import algebra
 from tensorAlgebra import tensorAlgebra
 from vector import vector
-
+from chainMaps import b_n, m_2, k_2, m_1
+from algebrasOfInterest import A,x1,x2,x3,x4,bases,Aq
